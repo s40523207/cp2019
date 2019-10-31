63fa4280c1cf9d492a36630b723b59a7cafd3182
s40523207 <40523207@gm.nfu.edu.tw>
2019-10-31 16:02:44 +0800

    blog settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40523207'
+SITENAME = 'CP2019 計算機程式 40523207'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
